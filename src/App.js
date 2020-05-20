7571cbb6ffc35d6098e9987a951cdfe3e7b6ff35
Eddy-123 <57044627+Eddy-123@users.noreply.github.com>
2020-05-20 22:41:46 +0100

    new test
    new test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Test CodeSandBox</h1>
+      <h1>CodeSandBox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
