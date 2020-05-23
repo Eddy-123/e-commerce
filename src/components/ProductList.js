5e380d2cf51d54b84567a6ee3e7bdc5569f176dd
Eddy-123 <57044627+Eddy-123@users.noreply.github.com>
2020-05-23 22:19:51 +0100

    title component created

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data.js";
 
 export default class ProductList extends Component {
+  state = {
+    products: storeProducts
+  };
   render() {
+    console.log(storeProducts);
     return (
-      <div>
-        <Product />
-      </div>
+      <React.Fragment>
+        <div className="my-5">
+          <div className="container">
+            <div className="row">
+              <Title name="our" title="product" />
+              {/*<Product />*/}
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
