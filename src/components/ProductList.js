17ce993071cf61ff71dfc216c9bcfc87983752cd
Eddy-123 <57044627+Eddy-123@users.noreply.github.com>
2020-05-24 09:12:12 +0100

    listing products on products page with images

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
-import { storeProducts } from "../data.js";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
-  state = {
-    products: storeProducts
-  };
   render() {
-    console.log(storeProducts);
     return (
       <React.Fragment>
         <div className="my-5">
           <div className="container">
             <div className="row">
               <Title name="our" title="product" />
-              {/*<Product />*/}
+            </div>
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => (
+                    <Product key={product.id} product={product} />
+                  ));
+                }}
+              </ProductConsumer>
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { ProductProvider } from "./context";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ProductProvider>
+      <Router>
+        <App />
+      </Router>
+    </ProductProvider>
   </React.StrictMode>,
   rootElement
 );
