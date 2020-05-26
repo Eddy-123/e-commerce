762abaa87cdf1935f9402a979f0477053d4f13b8
Eddy-123 <57044627+Eddy-123@users.noreply.github.com>
2020-05-26 12:22:34 +0100

    basic details page

--- src/context.js
+++ src/context.js
@@ -5,15 +5,27 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct
   };
-
+  componentDidMount() {
+    this.setProducts();
+  }
   handleDetail = () => {
     console.log("hello from detail");
   };
   addToCart = () => {
     console.log("hello from add to cart");
+  };
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
   };
   render() {
     return (
