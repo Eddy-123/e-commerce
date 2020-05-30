e6be7d04fc24a497c00385d53edc69148723e1ca
Eddy-123 <57044627+Eddy-123@users.noreply.github.com>
2020-05-30 23:12:45 +0100

    CartTotals component created

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CartColumns() {
   return (
-    <div className="container-fluid text-center d-lg-block">
+    <div className="container-fluid text-center d-none d-lg-block">
       <div className="row">
         <div className="mx-auto col-2">
           <p className="text-uppercase">products</p>
